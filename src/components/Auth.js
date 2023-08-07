ec1734fedb466388570bf8b9ce6b2ec449ea7665
Gourav <Gouravkumar.1273@gmail.com>
2023-08-07 17:27:53 +0530

    Auth Reducer

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,9 +1,16 @@
-import classes from './Auth.module.css';
-
+import { useSelector,useDispatch } from 'react-redux';
+import { authActions } from '../store';
+import classes from './Auth.module.css'; 
 const Auth = () => {
+const auth =useSelector(state=>state.auth.isAuthenticated);
+const dispatch=useDispatch();
+  console.log(auth);
+  const loginHandler=()=>{
+    dispatch(authActions.login())
+  }
   return (
     <main className={classes.auth}>
-      <section>
+     {!auth&& <section>
         <form>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
@@ -13,9 +20,9 @@ const Auth = () => {
             <label htmlFor='password'>Password</label>
             <input type='password' id='password' />
           </div>
-          <button>Login</button>
+          <button onClick={loginHandler}>Login</button>
         </form>
-      </section>
+      </section>}
     </main>
   );
 };
